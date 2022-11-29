65caf4544b4f5e64d948d91e831075d3c48a0316
Ash king <88268317+Ashking92@users.noreply.github.com>
2022-11-29 21:35:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['917385066631'] //ur owner number
+global.ownername = "Creator boy deva" //ur owner name
+global.ytname = "YT: Ash" //ur yt chanel name
+global.socialm = "GitHub: Ashking92" //ur github or insta name
+global.location = "India, Maharashtra, boisar" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['917385066631']
+global.ownertag = '919922356230' //ur tag number
+global.botname = 'Deva Bot' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.websitex = "https://sites.google.com/view/technicalash" //ur website to be displayed
+global.botscript = 'https://github.com/Ashking92/' //script link
+global.reactmoji = "✨️" //ur menu react emoji
+global.themeemoji = "🥀" //ur theme emoji
+global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🧑‍💻🧑‍💻🧑‍💻 " //ur sticker watermark packname
+global.author = "Creator boy deva" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['917385066631'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
